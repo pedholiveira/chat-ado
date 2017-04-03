85a813ec9377fa1f0b6ee4accca812da18d1598a
Luiz Felipe Marques de Oliveira <Luiz Felipe Marques de Oliveira>
2017-04-02 23:18:37 -0300

    Mudança na lógica de atualização do código da mensagem(msgNr)

--- client/assets/js/controllers/ChatiadoController.js
+++ client/assets/js/controllers/ChatiadoController.js
@@ -2,23 +2,24 @@ class ChatiadoController {
     constructor() {
         let $ = document.querySelector.bind(document);
 
-	this._inputUser = $('.caixa-busca input');
+	    this._inputUser = $('.caixa-busca input');
 
         this._inputMessage = $('textarea');
         this._inputMessage.focus();
 
         this._selectedUser = $('.caixa-conversa .contato');
 
-	this._messagesView = new MessagesView($('.caixa-conversa ul'));
+	    this._messagesView = new MessagesView($('.caixa-conversa ul'));
 
         this._usersView = new UsersView($('.lista-contatos'));
 
-	this._openedTalk = new Talk(new User('Sistema'));
-	this._talks = new Talks();
-	this._talks.add(this._openedTalk);
-	this._usersView.update(this._talks.users);
+        this._openedTalk = new Talk(new User('Sistema'));
+        this._talks = new Talks();
+        this._talks.add(this._openedTalk);
+        this._usersView.update(this._talks.users);
 
-        //refatorar
+        this._lastNumber;
+
         this._socket    = this._configSocket();
         this._userName  = username;
     }
@@ -48,18 +49,18 @@ class ChatiadoController {
 	}	
 
     selectUser(event) {
-	let userName = event.target.querySelector('span').textContent;
-	this._openedTalk = this._talks.getTalk(new User(userName));
-	this._messagesView.update(this._openedTalk.messages);
-	this._selectedUser.innerHTML = event.target.innerHTML;
+        let userName = event.target.querySelector('span').textContent;
+        this._openedTalk = this._talks.getTalk(new User(userName));
+        this._messagesView.update(this._openedTalk.messages);
+        this._selectedUser.innerHTML = event.target.innerHTML;
     }
 
     addMessage(event) {
         event.preventDefault();
 
-        let message = new Message(this._userName, this._openedTalk.messages.lastNumber, this._userName, this._openedTalk.user.name, this._inputMessage.value);
+        let message = new Message(this._userName, this._lastNumber, this._userName, this._openedTalk.user.name, this._inputMessage.value);
         this._openedTalk.messages.add(message);
-        this._messagesView.update(this._openedTalk.messages, 'enviada');
+        this._messagesView.update(this._openedTalk.messages);
         this._sendMessage(message);
 
         this._cleanForm();
@@ -75,31 +76,31 @@ class ChatiadoController {
             });
         });
         socket.on('data', data => {
-		if (data.data) {
-			data.data.forEach(m => {
-				let message = new Message(this._userName, data.msgNr, m.src, this._userName, m.data);
-				let talk = this._talks.getTalk(new User(m.src));
-				if(!talk) {
-					talk = new Talk(new User(m.src));
-					this._talks.add(talk);
-					this._usersView.update(this._talks.users);
-				}
-				talk.messages.add(message);
-				if(this._openedTalk === talk) {
-					this._messagesView.update(this._openedTalk.messages);
-				}
-			});
-		}
+            if (data.data) {
+                data.data.forEach(m => {
+                    let message = new Message(this._userName, data.msgNr, m.src, this._userName, m.data);
+                    let talk = this._talks.getTalk(new User(m.src));
+                    if(!talk) {
+                        talk = new Talk(new User(m.src));
+                        this._talks.add(talk);
+                        this._usersView.update(this._talks.users);
+                    }
+                    talk.messages.add(message);
+                    if(this._openedTalk === talk) {
+                        this._messagesView.update(this._openedTalk.messages);
+                    }
+                });
+            }
+            this._lastNumber = data.msgNr;
         });
 
-	setInterval(() => {
-		socket.emit('data', {
-			"cmd": "receber",
-			"id": this._userName,
-			"msgNr": 0
-		});
-	}, 5000);
-
-        return socket;
-    }
+        setInterval(() => {
+            socket.emit('data', {
+                "cmd": "receber",
+                "id": this._userName,
+                "msgNr": this._lastNumber
+            });
+        }, 5000);
+            return socket;
+        }
 }
